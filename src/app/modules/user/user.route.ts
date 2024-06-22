77e2e3b38d7a7c58c820c42090595d8ff13a57c9
unknown <antu.haks@gmail.com>
2024-06-22 15:17:13 +0600

    Deployement Changes

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,15 +1,5 @@
 import express from "express";
-import { UserControllers } from "./user.controller";
-import requestValidator from "../../middlewares/validateRequest";
-import { UserValidation } from "./user.validation";
-import auth from "../../middlewares/auth";
 
 const router = express.Router();
-
-// router.post('/signup',
-//     requestValidator(UserValidation.createUserValidationSchema),
-//     UserControllers.signUpUser);
-
-router.get("/", UserControllers.getAllUsers);
 
 export const UserRoutes = router;
